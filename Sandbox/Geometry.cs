3eaf41b3b858aa2cbcbcceecb30bb84ff3ad7c16
Martin Kierkegaard <mark@easj.dk>
2016-10-03 20:36:32 +0200

    solution for the exercise

--- Sandbox/Geometry.cs
+++ Sandbox/Geometry.cs
@@ -7,5 +7,49 @@ namespace Sandbox
 {
     public class Geometry
     {
+        // Note that this class does not need any instance fields
+        // (why...?)
+
+        // Constructor
+        public Geometry()
+        {
+        }
+
+        // Calculate perimeter of a rectangle
+        public double CalculateRectanglePerimeter(double sideA, double sideB)
+        {
+            double perimeter = 2 * sideA + 2 * sideB;
+            return perimeter;
+        }
+
+        // Calculate area of a rectangle
+        public double CalculateRectangleArea(double sideA, double sideB)
+        {
+            double area = sideA * sideB;
+            return area;
+        }
+
+        // Calculate area of a circle
+        public double CalculateCircleArea(double radius)
+        {
+            // PI is assumed to be 3.1416
+            double area = 3.1416 * radius * radius;
+            return area;
+        }
+
+        // Check if the given polygon is a rectangle (i.e. if all angles are 90 degrees)
+        public bool IsPolygonARectangle(int angleA, int angleB, int angleC, int angleD)
+        {
+            bool isRectangle = (angleA == 90) && (angleB == 90) && (angleC == 90) && (angleD == 90);
+            return isRectangle;
+        }
+
+        // Given two angles of a triangle, calculate the last angle
+        // Use the fact that the sum of the angles in a triangle is 180
+        public int CalculateThirdAngleOfTriangle(int angleA, int angleB)
+        {
+            int angleC = 180 - angleA - angleB;
+            return angleC;
+        }
     }
 }
--- Sandbox/InsertCodeHere.cs
+++ Sandbox/InsertCodeHere.cs
@@ -8,6 +8,36 @@ namespace Sandbox
         {
             // The FIRST line of code should be BELOW this line
 
+            // Create a Geometry object
+            Geometry geo = new Geometry();
+
+            // Test it!
+            //
+            Console.WriteLine("Test using rectangle with side lengths 8 and 14...");
+            Console.WriteLine("Perimeter is {0}", geo.CalculateRectanglePerimeter(8, 14));
+            Console.WriteLine("Area is {0}", geo.CalculateRectangleArea(8, 14));
+
+            Console.WriteLine("Test using rectangle with side lengths 2 and 21...");
+            Console.WriteLine("Perimeter is {0}", geo.CalculateRectanglePerimeter(2, 21));
+            Console.WriteLine("Area is {0}", geo.CalculateRectangleArea(2, 21));
+
+            Console.WriteLine("Test using circle with radius 7...");
+            Console.WriteLine("Area is {0}", geo.CalculateCircleArea(7));
+
+            Console.WriteLine("Test using circle with radius 30...");
+            Console.WriteLine("Area is {0}", geo.CalculateCircleArea(30));
+
+            Console.WriteLine("Test using polygon with angles 90, 90, 100 and 80");
+            Console.WriteLine("Polygon is a rectangle : {0}", geo.IsPolygonARectangle(90, 90, 100, 80));
+
+            Console.WriteLine("Test using polygon with angles 90, 90, 90 and 90");
+            Console.WriteLine("Polygon is a rectangle : {0}", geo.IsPolygonARectangle(90, 90, 90, 90));
+
+            Console.WriteLine("Test using triangle with angles 20 and 80");
+            Console.WriteLine("Last angle is {0}", geo.CalculateThirdAngleOfTriangle(20, 80));
+
+            Console.WriteLine("Test using triangle with angles 50 and 105");
+            Console.WriteLine("Last angle is {0}", geo.CalculateThirdAngleOfTriangle(50, 105));
 
             // The LAST line of code should be ABOVE this line
         }
